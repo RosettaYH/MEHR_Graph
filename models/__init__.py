ba5739cf8996f3c43b2bd62156b936842ecbee04
Rosetta Hu <rosettahu@mac.mynetworksettings.com>
2025-05-06 13:35:22 -0400

    Remove ignored files from repository

--- models/__init__.py
+++ models/__init__.py
@@ -3,18 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 from .GCN import GCN
-# from .BGCN import BGCN
 from .GAT import GAT
-# from .GIN import GIN
-# from .HAN import HAN
 from .HGT import HGT
-# from .HetRGCN import HeteroRGCN
 
-__all__ = [
-    'GCN',
-    'GAT',
-    # 'GIN',
-    # 'HAN',
-    'HGT',
-    # 'HeteroRGCN'
-]
+__all__ = ['GCN', 'GAT', 'HGT']
